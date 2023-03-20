a588dd0d27546f42b0cdc11e451187ab192f88aa
minhbac01job <minhbac01.job@gmail.com>
2023-03-20 13:44:40 +0700

    remove generate css folder

--- src/index.js
+++ src/index.js
@@ -1,30 +1,36 @@
-const express = require('express');
-const path = require('path');
-const morgan = require('morgan');
-const handlebars = require('express-handlebars');
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+const handlebars = require("express-handlebars");
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '/public')));
+const route = require("./routes");
 
-app.use(morgan('combined'));
+app.use(express.static(path.join(__dirname, "/public")));
 
-// Template engine
-app.engine('hbs', handlebars.engine({
-  extname: '.hbs'
-}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.json());
 
-//route
-app.get('/', (req, res) => {
-  res.render('home');
-})
+//app.use(morgan("combined"));
 
-app.get('/news', (req, res) => {
-  res.render('news');
-})
+// Template
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "resources/views"));
+
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-})
+});
